89afddbcea7b63924a28b928f86a343f74202e28
Var <146992931+IfVar@users.noreply.github.com>
2023-11-16 12:46:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Variables */
+    const txt = document.getElementById("banana");
+    var x = 1;
+
+    const a = document.body;
+
+/* Event Listeners */
+document.addEventListener("DOMContentLoaded", (event) => {
+    // Script To Load Automatically Once Website Loads
+});
+
+function ScriptO(){
+    txt.style.color = "white";
+    txt.innerText =+ x++;
+}
+
+Onclick(a, function(){
+    alert('hi');
+});
+
+/* Functions */
+    // Content Here
+
+/* Other Scripts */
+    // Content Here
